db4139fec582cb5c609d0c4633c25eba686acf78
AlexisAlb98 <alexisalbarracin2014@gmail.com>
2024-04-24 18:13:58 -0300

    commit de Alexis

--- E-Commerce/E-Commerce/obj/Debug/net6.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/E-Commerce/obj/Debug/net6.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef039081190bfe0aa284714497ef42eec082cd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
